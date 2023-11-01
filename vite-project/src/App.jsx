2ceeca83b365ccf67484e3ce3184d95a61e3a169
rayen bakali <rayen.bakali@gmail.com>
2023-11-01 15:29:53 +0100

    switch tab simple try

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const content = [
+[
+  "salem alaykom",
+  "hedhi first section",
+  "beslema"
+],
+[ "salem alaykom",
+"hedhi second section",
+"beslema"],
+[ "salem alaykom",
+"hedhi third section",
+"beslema"]
+  ];
+
+  const[currentIndex,setCurrentIndex]=useState(0)
+  return(
+<>
+<h1>Choose your section</h1>
+  <button className={currentIndex===0? "active":""} onClick={()=>setCurrentIndex(0)}>First Section?</button>
+  <button className={currentIndex===1? "active":""} onClick={()=>setCurrentIndex(1)}>Second Section?</button>
+  <button className={currentIndex===2? "active":""} onClick={()=>setCurrentIndex(2)}>Third Section?</button>
+  {content[currentIndex].map(e=>
+  <li></li>
+ )}
+ </>
+
+ )
+}
+
+export default App

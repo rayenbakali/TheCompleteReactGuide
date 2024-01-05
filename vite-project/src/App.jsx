fce0a8d59f6a4f492d64e9d6fb3311226c858bf5
Rayen <rayen.bakali@gmail.com>
2024-01-05 17:44:26 +0100

    updates

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,26 +1,43 @@
-const reactDescription = ["Fundamnental ","Crucial","Core"]
-function getRandomInt(max){
-  return Math.floor(Math.random()*(max+1))
-}
-function Header(){
-  const dynamictext= reactDescription[getRandomInt(2)]
-  return (<header>
-  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-  <h1>React Essentials</h1>
-  <p>
-    {dynamictext} React concepts you will need for almost any app you are
-    going to build!
-  </p>
-</header>)   
-}
- 
-function App() {
+import ReactImg from "./assets/react-core-concepts.png";
 
+const reactDescription = ["Fundamnental ", "Crucial", "Core"];
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept() {
+  return (
+    <li>
+      <img src="#" alt="" />
+      <h3>Title</h3>
+      <p>Description</p>
+    </li>
+  );
+}
+function Header() {
+  const RandomValue = reactDescription[getRandomInt(2)];
+  return (
+    <header>
+      <img src={ReactImg} alt="image" />
+      <h1>React Essentials</h1>
+      <p>
+        {RandomValue} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
   return (
     <div>
-     <Header/>
+      <Header />
       <main>
-        <h2>Time to get started with this shit!</h2>
+        <section id="core-concepts">
+          <CoreConcept />
+          <h2>Core Concepts</h2>
+          <ul></ul>
+        </section>
       </main>
     </div>
   );
